e011ee9cdf154854fdd29ce3746b37d2cfa22148
Niv Sardi <xaiki@evilgiggle.com>
2014-09-11 18:11:34 -0300

    oauth2 authinfo
    Signed-off-by: Niv Sardi <xaiki@evilgiggle.com>

--- mail-scripts/authinfo.py
+++ mail-scripts/authinfo.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python
 import re, os
 
-
 def get_password(machine, login, port=''):
     mc = re.compile("machine %s" % machine)
     po = re.compile("port %s" % port)
@@ -11,10 +10,10 @@ def get_password(machine, login, port=''):
     authinfo = os.popen("gpg -q --use-agent --no-tty -d ~/.authinfo.gpg").read()
     for l in authinfo.split('\n'):
         if (mc.search(l) and lg.search(l)):
-            return pw.search(l).group(1)
+            return pw.search(l).group(1).strip()
 
     return None
 
 if __name__ == '__main__':
     import sys
-    sys.stdout.write(get_password(sys.argv[1], sys.argv[2], sys.argv[3]).strip())
+    sys.stdout.write(get_password(sys.argv[1], sys.argv[2], sys.argv[3]))
---
+++ mail-scripts/oauth2-authinfo.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+from authinfo import get_password
+from oauth2 import GenerateOAuth2String,RefreshToken
+
+def get_token (machine, login, port=''):
+    tokens = get_password ('oauth2.' + machine, login, port).split(':')
+    response =  RefreshToken(tokens[0], tokens[1], tokens[2])
+    auth = GenerateOAuth2String (login, response['access_token'])
+    import sys
+    sys.stderr.write('will auth: ' + auth + '\n');
+    return auth
+
+if __name__ == '__main__':
+    import sys
+    sys.stdout.write(get_token(sys.argv[1], sys.argv[2], sys.argv[3]));
