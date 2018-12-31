8647d704dc431a828a648e58d2388395e3665595
Niv Sardi <xaiki@evilgiggle.com>
2018-12-31 16:06:13 -0300

    add fzf and move all config files to conf.d
    Signed-off-by: Niv Sardi <xaiki@evilgiggle.com>

---
+++ config/fish/conf.d/40_keybindings.fish
@@ -0,0 +1,1 @@
+bind \cr '__fzf_reverse_isearch'
--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,7 +1,5 @@
 #!/usr/bin/env fish
 set fish_greeting
 
-source ~/.config/fish/10_env.fish
-source ~/.config/fish/20_aliases.fish
-source ~/.config/fish/30_prompt.fish
+source ~/.config/fish/conf.d/*.fish
 source ~/.config/fish/(hostname).fish
