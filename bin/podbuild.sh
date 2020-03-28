b276a8689bbdbc6efdf21f24e5135a644d01da3a
Niv Sardi <xaiki@evilgiggle.com>
2020-03-28 16:46:37 -0300

    add pods and bin
    Signed-off-by: Niv Sardi <xaiki@evilgiggle.com>

---
+++ bin/podbuild.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+set -e
+set -x
+
+apg_container=apg
+xelatex_container=xelatex
+pdftk_container=mnuessler/pdftk
+
+
+filename=`realpath ${1}`
+pw=`podman run apg_container -n1`
+realname=`echo ${1} | sed s/.org$//`
+realfullname=`echo ${filename} | sed s/.org$//`
+basename=`basename ${realfullname}`
+dirname=`dirname ${1}`
+
+cmd="xelatex -interaction nonstopmode ${basename}.tex"
+
+flatpak run --command='emacsclient' org.gnu.emacs -e "(save-excursion
+        (setq org-latex-compiler \"xelatex\")
+        (find-file \"${filename}\")
+        (org-export-to-file 'latex \"${realfullname}.tex\" nil nil nil nil nil))"
+podman run -ti -v ${PWD}:/data:Z ${xelatex_container} sh -c "(cd ${dirname}; ${cmd}; ${cmd};)"
+podman run -v ${PWD}:/work:Z ${pdftk_container} ${realname}.pdf output ${realname}.crypt.pdf owner_pw ${pw}
