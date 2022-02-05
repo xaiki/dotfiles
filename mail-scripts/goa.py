0b6e2230fcd6e34c064e4df6d98b03c17f79ae0c
Niv Sardi <xaiki@evilgiggle.com>
2022-02-05 20:38:45 -0300

    implement user in goa

--- mail-scripts/goa.py
+++ mail-scripts/goa.py
@@ -5,16 +5,16 @@ gi.require_version('Goa', '1.0')
 
 from gi.repository import Goa
 
-client = Goa.Client.new_sync()
-mail_handles = [h  for h in client.get_accounts() if h.get_mail()]
+def lookup(host, user):
+    client = Goa.Client.new_sync()
+    mail_handles = [h  for h in client.get_accounts() if h.get_mail()]
 
-for s in sys.argv[1:]:
-    found = False
     for h in mail_handles:
         m = h.get_mail()
-        if m.get_property("imap-host") == s:
+        if m.get_property("imap-host") == host and m.get_property("imap-user-name") == user:
             print(h.get_password_based().call_get_password_sync("imap-password"))
-            found = True
-            break
-    if not found:
-        print(f"{s} not found")
+            return
+    print(f"{user} (at) {host} not found")
+
+[h, u] = sys.argv[1:]
+lookup(h, u)
